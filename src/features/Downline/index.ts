344c92375e181954c4462970a0cc6aaa747ae457
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-08-24 12:19:07 +0100

    build: absolute route setup

--- src/features/Downline/index.ts
+++
@@ -1,8 +0,0 @@
-import Transactions from './Downline.Module';
-export * from './Downline.Context';
-export * from './api';
-export * from './hooks';
-export * from './Downline.Module';
-export * from './components';
-
-export { Transactions };
